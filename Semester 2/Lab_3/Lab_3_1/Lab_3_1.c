608f314cce242b630f0749d030d4c84ac4e3453b
BlednyJ <rendemon.zero@gmail.com>
2025-04-19 23:43:57 +0200

    Rewrote file hierarchy Moved Lab_3_1.c to Lab_3_1 directory Wrote task Lab_3_1.c Created and wrote task Lab_3_2.c

---
+++ Semester 2/Lab_3/Lab_3_1/Lab_3_1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int digit_seen_counted[10] = {0};
+
+int check_repeated_digits(long n);
+
+int count_repeated_digits(long n)
+{
+    int digit;
+    while (n > 0) {
+        digit = n % 10;
+        digit_seen_counted[digit] += 1;
+        n /= 10;
+    }
+    return n;
+}
+
+void write_repeated_digits()
+{
+    for (long i = 0; i < 10; i++)
+    {
+        if (digit_seen_counted[i] > 1) printf("%d (%d разів)\n", i, digit_seen_counted[i]);
+    }
+}
+
+int main()
+{
+    long n;
+    printf("Введіть число: ");
+    scanf("%ld", &n);
+    if (check_repeated_digits(n))
+    {
+        count_repeated_digits(n);
+        printf("Є цифри, що повторюються\n");
+        write_repeated_digits();
+    }
+    else
+        printf("Немає цифр, що повторюються\n");
+    return 0;
+}
+
+int check_repeated_digits(long n)
+{
+    int digit_seen[10] = {0};
+    int digit;
+    while (n > 0) {
+        digit = n % 10;
+        if (digit_seen[digit])
+            break;
+        digit_seen[digit] = 1;
+        n /= 10;
+    }
+    return n;
+}
---
+++ Semester 2/Lab_3/Lab_3_2/Lab_3_2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void write_by_keyboard(int arr[], unsigned int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Введіть %d-й елемент масиву:", i);
+        scanf("%d", &arr[i]);
+    }
+}
+
+void write_by_random(int arr[], unsigned int size)
+{
+    for (int i = 0; i < size; i++) arr[i] = rand();
+}
+
+int minimalNumberFind(int arr[], unsigned int size)
+{
+    int min = arr[0];
+    for (int i = 1; i < size; i++) if (arr[i] < min) min = arr[i];
+    return min;
+}
+
+int oddSumBeforeMinimal(int arr[], int min)
+{
+    int sum = arr[0];
+    for (int i  = 0; i < min; i += 2) sum += arr[i];
+    return sum;
+}
+
+void temp_writeDown(int arr[], unsigned int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
+int main()
+{
+    unsigned int N;
+    int choice;
+    printf("Введіть кількість елементів масиву: ");
+    scanf("%d", &N);
+    if (N > 100)
+    {
+        printf("Помилка: кількість елементів масиву не може бути більшим за 100\n");
+        return 1;
+    }
+    int* numberArr = (int*)malloc(N * sizeof(int));
+    printf("Ввести елементи масиву з клавіатури (введіть 0) чи випадковим чином (введіть 1): ");
+    scanf("%d", &choice);
+    if (choice == 0) write_by_keyboard(numberArr, N);
+    else if (choice == 1) write_by_random(numberArr, N);
+    else
+    {
+        printf("Помилка: Введено інше число!\n");
+        return 1;
+    }
+    temp_writeDown(numberArr, N);
+    int minimalNumber = minimalNumberFind(numberArr, N);
+    printf("%d", minimalNumber);
+    if (minimalNumber == numberArr[0])
+    {
+        printf("Перше число є мінімальним! Розрахунок неможливий!\n");
+        return 1;
+    }
+    else printf("Сума елементів масиву с непарними індексами, що знаходяться до мінімального елемента: %d\n", oddSumBeforeMinimal(numberArr, minimalNumber));
+    return 0;
+}

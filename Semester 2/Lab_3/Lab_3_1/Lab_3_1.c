dc8431db22a768f784b545ff958c6265dc116cb2
BlednyJ <rendemon.zero@gmail.com>
2025-04-22 19:23:37 +0200

    Lab_3_1.c - rewrote for tests Lab_3_1_test.cpp - don't work. I'm so fucking  mad and tired of this shit. Kill me

--- Semester 2/Lab_3/Lab_3_1/Lab_3_1.c
+++ Semester 2/Lab_3/Lab_3_1/Lab_3_1.c
@@ -1,42 +1,52 @@
 #include <stdio.h>
 
-int digit_seen_counted[10] = {0};
+int check_repeated_digits(long n);
 
-int check_and_count_repeated_digits(long n);
+int count_repeated_digits(long n, int counted[])
+{
+    int digit;
+    if (n < 0) n *= -1;
+    while (n > 0) {
+        digit = n % 10;
+        counted[digit] += 1;
+        n /= 10;
+    }
+}
 
-void write_repeated_digits()
+void write_repeated_digits(int arr[])
 {
     for (long i = 0; i < 10; i++)
     {
-        if (digit_seen_counted[i] > 1) printf("%d (%d разів)\n", i, digit_seen_counted[i]);
+        if (arr[i] > 1) printf("%d (%d разів)\n", i, arr[i]);
     }
 }
 
 int main()
 {
     long n;
+    int digit_seen_counted[10] = {0};
     printf("Введіть число: ");
     scanf("%ld", &n);
-    if (check_and_count_repeated_digits(n))
+    if (check_repeated_digits(n))
     {
         printf("Є цифри, що повторюються:\n");
-        write_repeated_digits();
+        count_repeated_digits(n, digit_seen_counted);
+        write_repeated_digits(digit_seen_counted);
     }
     else
         printf("Немає цифр, що повторюються\n");
     return 0;
 }
 
-int check_and_count_repeated_digits(long n)
-{
+int check_repeated_digits(long n){
+    int digit_seen[10] = {0};
     int digit;
-    int isRepeated = 0;
-    if (n < 0) n *= -1;
     while (n > 0) {
         digit = n % 10;
-        if (digit_seen_counted[digit])  isRepeated = 1;
-        digit_seen_counted[digit] += 1;
+        if (digit_seen[digit]) /* Якщо така цифра вже є */
+            break; /* то це означає, що є цифри, що повторюються */
+        digit_seen[digit] = 1;
         n /= 10;
     }
-    return isRepeated;
+    return n;
 }
---
+++ Semester 2/Lab_3/Lab_3_1/Lab_3_1_test.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+extern "C" {
+#include "Lab_3_1.c"
+}
+
+TEST(PositiveDigitTest, CountRepeatedDigits) {
+    int counted[10] = {0};
+    
+    count_repeated_digits(1102233474555, counted);
+
+    EXPECT_EQ(counted[0], 1);
+    EXPECT_EQ(counted[1], 2);
+    EXPECT_EQ(counted[2], 2);
+    EXPECT_EQ(counted[3], 2);
+    EXPECT_EQ(counted[4], 2);
+    EXPECT_EQ(counted[5], 3);
+    EXPECT_EQ(counted[6], 0);
+    EXPECT_EQ(counted[7], 1);
+    EXPECT_EQ(counted[8], 0);
+    EXPECT_EQ(counted[9], 0);
+}
+
+TEST(NegativeDigitTest, CountRepeatedDigits) {
+    int counted[10] = {0};
+
+    count_repeated_digits(-27735300, counted);
+
+    EXPECT_EQ(counted[0], 2);
+    EXPECT_EQ(counted[1], 0);
+    EXPECT_EQ(counted[2], 1);
+    EXPECT_EQ(counted[3], 2);
+    EXPECT_EQ(counted[4], 0);
+    EXPECT_EQ(counted[5], 1);
+    EXPECT_EQ(counted[6], 0);
+    EXPECT_EQ(counted[7], 2);
+    EXPECT_EQ(counted[8], 0);
+    EXPECT_EQ(counted[9], 0);
+}

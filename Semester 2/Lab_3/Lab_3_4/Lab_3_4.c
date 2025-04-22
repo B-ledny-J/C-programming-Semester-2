1f4ddd858d28574d8b65473c9eb99ffbae52f00f
BlednyJ <rendemon.zero@gmail.com>
2025-04-22 18:08:36 +0200

    Lab_3_4.c - well, all shit

---
+++ Semester 2/Lab_3/Lab_3_4/Lab_3_4.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_SUITS 4 /* кількість мастей */
+#define NUM_RANKS 13 /* кількість рангів */
+
+int any_three(int arr[NUM_SUITS][NUM_RANKS])
+{
+    int count = 0;
+    for (int j = 0; j < NUM_RANKS; j++)
+    {
+        for (int i = 0; i < NUM_SUITS; i++) if (arr[i][j] == 1) count++;
+        if (count >= 3) return 1;
+        count = 0;
+    }
+    return 0;
+}
+
+int main()
+{
+    /* карти на руках */
+    int player_hand[NUM_SUITS][NUM_RANKS], in_hand[NUM_SUITS][NUM_RANKS] = {0};
+    int num_players = 3, num_cards = 6, rank, suit;
+    const char rank_code[] = {'2','3','4','5','6','7','8','9','t','j','q','k','a'};
+    const char suit_code[] = {'c','d','h','s'};
+    /* ініціалізация генератора випадкових чисел */
+    srand(time(0));
+    for (int i = 0; i < num_players; i++)
+    {
+        printf("Картки гравця %d:", i + 1);
+        for (int j = 0; j < num_cards;)
+        {
+            /* обрання випадкової масті */
+            suit = rand() % NUM_SUITS;
+            /* обрання випадкового рангу */
+            rank = rand() % NUM_RANKS;
+            if (!in_hand[suit][rank])
+            {
+                in_hand[suit][rank] = 1; /* карта обрана */
+                player_hand[suit][rank] = 1;
+                printf(" %c%c", rank_code[rank],suit_code[suit]);
+                j++;
+            }
+        }
+        printf("\n");
+        printf("Чи є у гравця %d комбінація \"трійка\": ", i + 1);
+        if (any_three(player_hand)) printf("так\n");
+        else printf("ні\n");
+        // обнулення масиву
+        for (int l = 0; l < NUM_SUITS; l++) {
+            for (int k = 0; k < NUM_RANKS; k++) {
+                player_hand[l][k] = 0;
+            }
+        }
+    }
+    return 0;
+}

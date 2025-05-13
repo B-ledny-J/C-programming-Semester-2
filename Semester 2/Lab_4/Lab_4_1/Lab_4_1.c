359ccdc0d8fbd9082fd053f20197947d30c327db
BlednyJ <rendemon.zero@gmail.com>
2025-05-13 21:31:15 +0200

    Wrote Lab_4_1.c Added Lab_4_3.c

--- Semester 2/Lab_4/Lab_4_1/Lab_4_1.c
+++ Semester 2/Lab_4/Lab_4_1/Lab_4_1.c
@@ -1,27 +1,70 @@
 /* Швидке сортування */
 #include <stdio.h>
-#define N 10
+#include <stdlib.h>
+//#define N 10
+
 /* функція швидкого сортування */
 void quicksort(int a[], int low, int high);
+
 /* функція розділу масива */
 int split(int a[], int low, int high);
+
+void write_by_keyboard(int arr[], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("Введіть %d елемент масиву для сортування:", i);
+        scanf("%d", &arr[i]);
+    }
+}
+
+void write_by_random(int arr[], int N, int range)
+{
+    // Для кращої читаності випадкові числа обмежені 300-ми числами
+    for (int i = 0; i < N; i++) arr[i] = rand()%range;
+}
+
+void writeArray(int arr[], int N)
+{
+    for (int i = 0; i < N; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
 /* функція main */
 int main()
 {
-    int a[N], i;
-    printf("Введіть %d чисел для сортування", N);
-    for (i = 0; i < N; i++) scanf("%d", &a[i]);
-    quicksort(a, 0, N - 1);
-    printf("Відсортовані числа: ");
-    for (i = 0; i < N; i++) printf("%d ", a[i]);
-    printf("\n");
+    int length;
+    int choice;
+    int range;
+    printf("Введіть кількість елементів масиву: ");
+    scanf("%d", &length);
+    int* a = (int*)malloc(length * sizeof(int));
+    printf("Ввести елементи масиву з клавіатури (введіть 0) чи випадковим чином (введіть 1): ");
+    scanf("%d", &choice);
+    if (choice == 0) write_by_keyboard(a, length);
+    else if (choice == 1)
+    {
+        printf("Введіть обмеження (максимальне випадкове число): ");
+        scanf("%d", &range);
+        write_by_random(a, length, range);
+    }
+    else
+    {
+        printf("Помилка: Введено інше число!\n");
+        return 1;
+    }
+    printf("Вигляд масиву: ");
+    writeArray(a, length);
+    quicksort(a, 0, length - 1);
+    printf("Вигляд відсортованного масиву: ");
+    writeArray(a, length);
     return 0;
 }
 
 void quicksort(int a[], int low, int high)
 {
   int middle;
-  if (low < high) return;
+  if (low >= high) return;
   middle = split(a, low, high);
   quicksort(a, low, middle - 1);
   quicksort(a, middle + 1, high);
@@ -32,10 +75,10 @@ int split(int a[], int low, int high)
     int part_element = a[low];
     for(;;)
     {
-        while (low < high && part_element <= a[high]) high--;
+        while (low < high && part_element >= a[high]) high--;
         if (low >= high) break;
         a[low++] = a[high];
-        while (low < high && a[low] <= part_element) low++;
+        while (low < high && a[low] >= part_element) low++;
         if (low >= high) break;
         a[high--] = a[low];
     }
